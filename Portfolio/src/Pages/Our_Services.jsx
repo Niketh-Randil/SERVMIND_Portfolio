7ea9600d7e974172ad7d16773fc5498070473c68
gimindugenuka <gimindu.20220282@iit.ac.lk>
2024-10-24 22:21:22 +0530

    css changers in our services

--- Portfolio/src/Pages/Our_Services.jsx
+++ Portfolio/src/Pages/Our_Services.jsx
@@ -7,32 +7,34 @@ const developmentApproachData = [
     title: "UX Design",
     description:
       "Unlike other companies, we are a UX first development company. Projects are driven by designers and they make sure design and experiences translate to code.",
-    //icon: { puk },
+    icon: { puk },
   },
   {
     title: "WebFlow Development",
     description:
       "Unlike other companies, we are a UX first development company. Projects are driven by designers and they make sure design and experiences translate to code.",
-    //icon: { puk },
+    icon: { puk },
   },
   {
     title: "Marketing",
     description:
       "Unlike other companies, we are a UX first development company. Projects are driven by designers and they make sure design and experiences translate to code.",
-    //icon: { puk },
+    icon: { puk },
   },
   {
     title: "Branding",
     description:
       "Unlike other companies, we are a UX first development company. Projects are driven by designers and they make sure design and experiences translate to code.",
-    //icon: { puk },
+    icon: { puk },
   },
 ];
 
 const ServiceCard = ({ icon, title, description }) => (
   <div className="service-card">
     <div className="service-card-content">
-      <div className="service-icon">{icon}</div>
+      <div className="service-icon">
+        <img src={icon.puk} alt={`${title} icon`} />
+      </div>
       <div className="text-content">
         <h3 className="service-title">{title}</h3>
         <p className="service-description">{description}</p>
@@ -44,7 +46,7 @@ const ServiceCard = ({ icon, title, description }) => (
 function OurServices() {
   return (
     <div className="services-container">
-      <h1 className="title services-title">Our Services</h1>
+      <h1 className="services-title">Our Services</h1>
       <div className="services-wrapper">
         <div className="services-grid">
           {developmentApproachData.map((item, index) => (
